c859e2d03d68e63e63e843a5b318085b4c6cea4f
HayleyLL <hayley.s.liu@gmail.com>
2020-07-23 12:25:36 +0800

    feat: add 'return to hall & quit the current game' function

--- src/components/recording/sider-header.jsx
+++ src/components/recording/sider-header.jsx
@@ -1,32 +1,59 @@
 import React from 'react';
+import {useHistory, useLocation} from 'react-router-dom';
 import {Button} from 'antd';
-import {LineOutlined,CloseOutlined } from '@ant-design/icons';
+import {LineOutlined, CloseOutlined} from '@ant-design/icons';
 import * as meta from '../../meta';
+import {baseUrl} from '../../end-point/httpRqst'
+import axios from 'axios'
+import Cookies from "js-cookies/src/cookies";
 
-const SiderHeader = (props)=>{
-    const {roomInfo}=props;
-    let status=meta.PREPARING;
-    if(roomInfo.status===meta.PREPARING){
-        status='游戏准备中'
-    }else if(roomInfo.status===meta.PLAYING){
-        status='游戏进行中'
-    }else{
-        status='游戏已结束'
+const playerId = Cookies.getItem("playerId");
+
+const SiderHeader = (props) => {
+    const {roomInfo} = props;
+    let status = meta.PREPARING;
+    if (roomInfo.status === meta.PREPARING) {
+        status = '游戏准备中'
+    } else if (roomInfo.status === meta.PLAYING) {
+        status = '游戏进行中'
+    } else {
+        status = '游戏已结束'
+    }
+
+    const history = useHistory();
+    const location = useLocation();
+    const roomId = location.pathname;
+
+    const handleReturn = () => {
+        history.push('/');
+    }
+
+    const handleExit = () => {
+        if (playerId !== roomInfo.blackHolder && playerId !== roomInfo.whiteHolder) {
+            handleReturn();
+        } else {
+            axios({
+                url: `${baseUrl}${roomId}/giveIn`,
+                method: 'put'
+            }).then(() => {
+                handleReturn()
+            }).catch(er => console.error(er))
+        }
     }
 
     return (
         <div className='sider-header'>
-           <div className='operation'>
-               <div className='room-status'>
-                   {status}
-               </div>
-               <Button className='room-btn'>
-                   <LineOutlined />
-               </Button>
-               <Button className='room-btn'>
-                   <CloseOutlined />
-               </Button>
-           </div>
+            <div className='operation'>
+                <div className='room-status'>
+                    {status}
+                </div>
+                <Button className='room-btn' onClick={handleReturn}>
+                    <LineOutlined/>
+                </Button>
+                <Button className='room-btn' onClick={handleExit}>
+                    <CloseOutlined/>
+                </Button>
+            </div>
             <div className='room-info'>
                 房间号：{props.roomInfo._id}
             </div>
